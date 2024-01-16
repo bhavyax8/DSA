fe62d2db4873efa8e782190a0bbbb54198f8ac08
bhavyax8 <96230446+bhavyax8@users.noreply.github.com>
2024-01-17 00:42:58 +0530

    Add files via upload

---
+++ pallindromicSeq.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class pallindromicSeq {
+    static Scanner sc = new Scanner(System.in);
+
+    public static boolean isPallindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i <= j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void Solution(String str){
+        int n= str.length();
+        for(int st =0; st<n; st++){
+            for(int end= st; end<n; end++){
+                String s = str.substring(st, end+1);
+                if(isPallindrome(s)){
+                    System.out.println(s);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String st= "GAATTC";
+        Solution(st);
+    }
+}
